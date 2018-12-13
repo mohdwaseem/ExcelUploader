e65ccc746e1c50d96d76d8fb317112f1fef54eae
Mohd Waseem <mohdwaseem488@hotmail.com>
2018-12-14 02:13:02 +0530

    Initial Commit

---
+++ ExcelUploder/test.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test.aspx.cs" Inherits="ExcelUploder.test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table>
+            <tr>
+                <td>Select File</td>
+                <td><asp:FileUpload ID="FileUpload1" runat="server" /></td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="Button1" OnClick="Button1_Click" runat="server" Text="Upload" />
+                </td>
+                <td>
+                     <asp:Button ID="Button2" OnClick="Button2_Click" runat="server" Text="Save" />
+                     <asp:Button ID="Button3" OnClick="Button3_Click" runat="server" Text="Cancel" />
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ ExcelUploder/test.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+
+namespace ExcelUploder
+{
+    [Serializable]
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public SqlConnection Connect()
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-GRTG11N;Initial Catalog=TestUploader;Integrated Security=True");
+            con.Open();
+            return con;
+        }
+        
+        public List<JobList> GetData()
+        {
+            List<JobList> jobs = new List<JobList>();
+            string query = "SELECT * FROM NIC";
+            SqlCommand cmd = new SqlCommand(query,Connect());
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                jobs.Add(new JobList {
+                    JobId= Convert.ToInt32(reader[0].ToString()),
+                    ProfId= Convert.ToInt32(reader[1].ToString())
+                });
+            }
+            return jobs;
+        }
+         
+        public List<JobList> Jobs
+        {
+            get { return (List<JobList>)ViewState["_jobs_"]; }
+            set { ViewState["_jobs_"] = value; }
+        }
+        public List<int> WrongData
+        {
+            get { return (List<int>)ViewState["_wrong_"]; }
+            set { ViewState["_wrong_"] = value; }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            bool hasHeader = true;
+            var allJobs = GetData();
+            List<JobList> correctJobs = new List<JobList>();
+            List<int> excelData = new List<int>();
+            List<int> notFound = new List<int>();
+            using (var pck = new OfficeOpenXml.ExcelPackage())
+            {
+                using (var stream = File.OpenRead(Server.MapPath(FileUpload1.PostedFile.FileName)))
+                {
+                    pck.Load(stream);
+                }
+                var ws = pck.Workbook.Worksheets.First();
+               
+                var startRow = hasHeader ? 2 : 1;
+                for (int rowNum = startRow; rowNum <= ws.Dimension.End.Row; rowNum++)
+                {
+                    var wsRow = ws.Cells[rowNum, 1, rowNum, ws.Dimension.End.Column];
+                   
+                    foreach (var cell in wsRow)
+                    {
+                        excelData.Add(Convert.ToInt32(cell.Text));
+                    }
+                }
+            }
+            //Check List Exist in Database or not
+            foreach (var item in excelData.Distinct())
+            {
+                var title = allJobs.Find(x => x.ProfId == item);
+                if (title!=null)
+                {
+                    correctJobs.Add(new JobList { JobId= title.JobId ,ProfId=title.ProfId});
+                }
+                else
+                {
+                    notFound.Add(item);
+                }
+            }
+            Jobs = correctJobs;
+            WrongData = notFound;
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            var jobs = Jobs;
+            var wrong = WrongData;
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            var jobs = Jobs;
+            var wrong = WrongData;
+        }
+    }
+    [Serializable]
+    public class JobList
+    {
+        public int JobId { get; set; }
+        public int ProfId { get; set; }
+    }
+}
